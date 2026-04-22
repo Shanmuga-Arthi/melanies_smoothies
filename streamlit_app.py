80c35408a534931789d1a752c5c190cd328c3b57
Shanmuga-Arthi <60651573+Shanmuga-Arthi@users.noreply.github.com>
2026-04-22 17:53:27 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,7 +11,7 @@ st.write(
 )
 
 name_on_order = st.text_input("Name on Smoothie")
-st.write("The name on your smoothie will be", name_on_order)
+st.write("The name on your smoothie will be:", name_on_order)
 
 #get the current credentials
 cnx = st.connection("snowflake")
